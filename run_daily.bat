d72cf578851b87fa32c34157c97464968330de52
wenshenghuan <51964987@qq.com>
2026-08-23 19:49:21 +0800

    update

--- run_daily.bat
+++ run_daily.bat
@@ -1,4 +1,4 @@
 @echo off
 rem Daily batch task: run scraper for all forums
 cd /d "%~dp0"
-"D:\biancheng\python\python3.11.4\python.exe" run_batch.py
+"D:\biancheng\python\python3.11.4\python.exe" run_batch.py --restart
